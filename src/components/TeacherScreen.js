ccf628a95113f91a70744169d5e585d6b38cac7b
tejutej <teju97teju@gmail.com>
2024-06-28 18:28:22 +0530

    teacher screen

--- src/components/TeacherScreen.js
+++ src/components/TeacherScreen.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+import SelectComponent from "./SelectComponent";
+import { StudentContext } from "../store/studentcontext";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 const TeacherScreen = () => {
+  const { studentsInfo, setStudentsInfo } = useContext(StudentContext);
+  
+  const fetchStudents = async () => {
+    const response = await axios
+      .get("/studentsinfo");
+      setStudentsInfo(response.data)
+      return response.data;
+  };
+
+  const { data, error, isLoading, isError } = useQuery({
+    queryKey: ['data'],
+    queryFn: fetchStudents,
+  });
+
+  // useEffect(() => {
+  //   data && setStudentsInfo(data);
+  // }, [data]);
+
   return (
     <div>
-      <h2>Hello, !</h2>
-      <p>This is Screen 1</p>
+      <h1>Login</h1>
+      <Button
+        variant="contained"
+        color="primary"
+        component={Link}
+        to="/teacher-screen"
+      >
+        Teacher
+      </Button>
+      { (
+        <SelectComponent label="Select your Name" options={data?.studentInfo} />
+      )}
+      <Button
+        variant="contained"
+        color="primary"
+        component={Link}
+        to="/student-screen"
+      >
+        Student
+      </Button>
     </div>
   );
 };
